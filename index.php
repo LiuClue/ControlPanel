a082b7cba8b2489f34c4d493079d2158e639f44b
LiuClue <dennisliu323@gmail.com>
2019-11-18 17:52:17 -0500

    comment fixed

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-  echo 'check23   ';
 	date_default_timezone_set("America/Toronto");
 	echo "The time is " . date("Y/m/d h:i:sa");
 	/* Database connection settings */
@@ -9,8 +8,8 @@
 	$db = 'db';
 	$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
 
-	$data1 = '';
-	$data2 = '';
+	$intensity = '';
+	$date = '';
 
 	//query to get data from the table
 	$sql = "SELECT * FROM `light` ORDER BY date DESC";
@@ -19,134 +18,114 @@
 	//loop through the returned data
 	while ($row = mysqli_fetch_array($result)) {
 
-		$data1 = $data1 . '"'. $row['intensity'].'",';
-		$data2 = $data2 . '"'. $row['date'].'",';
+		$intensity = $intensity . '"'. $row['intensity'].'",';
+		$date = $date . '"'. $row['date'].'",';
 	}
 
-	$data1 = trim($data1,",");
-	$data2 = trim($data2,",");
+	$intensity = trim($intensity,",");
+	$date = trim($date,",");
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
-		<title>Accelerometer data</title>
+		<title>Sensor Display</title>
 
-		<style type="text/css">			
-			body{
-				font-family: Arial;
-			    margin: 80px 100px 10px 100px;
-			    padding: 0;
-			    color: white;
-			    text-align: center;
-			    background: #555652;
-			}
-
-			.container {
-				color: #E8E9EB;
-				background: #222;
-				border: #555652 1px solid;
-				padding: 10px;
-			}
-		</style>
+		<link rel="stylesheet" type="text/css" href="/css/styles.css">
 
 	</head>
-
 	<body>	   
-	    <div class="container">	
-	    <h1>USE CHART.JS WITH MYSQL DATASETS</h1>       
+		<div class="container" style="width:100%; height:65vh;">	
+	    <h1>Light Sensor History</h1>
+	    
+	    <!--Set canvas for chart--> 
 			<canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
 
+	    <!--script to fill chart with data--> 
 			<script>
 				var ctx = document.getElementById("chart").getContext('2d');
-				var xlabels =[<?php echo $data1;?>];
-				var ylabels =[<?php echo $data2;?>];
-				ylabels[0] = new Date(ylabels[0]);
-				ylabels[1] = new Date(ylabels[1]);
-		    	console.log(ylabels[0]);
-		    	console.log(xlabels[0]);
+				var ylabels =[<?php echo $intensity;?>];
+				var xlabels =[<?php echo $date;?>];
 			  
-    			var myChart = new Chart(ctx, {
-        		type: 'line',
-		        data: {
-		            labels: ylabels,
-		            datasets: 
-		            	[{
-		            		data: [{
-			            		y: xlabels[0],
-		    					x: ylabels[0]},
-		    					{
-			            		y: xlabels[1],
-		    					x: ylabels[1]},
-		    					{
-			            		y: xlabels[2],
-		    					x: ylabels[2]},
-		    					{
-			            		y: xlabels[3],
-		    					x: ylabels[3]},
-		    					{
-			            		y: xlabels[4],
-		    					x: ylabels[4]},
-		    					{
-			            		y: xlabels[5],
-		    					x: ylabels[6]},
-		    					{
-			            		y: xlabels[7],
-		    					x: ylabels[7]},
-		    					{
-			            		y: xlabels[8],
-		    					x: ylabels[8]},
-		    					{
-			            		y: xlabels[9],
-		    					x: ylabels[9]},
-		    					{
-			            		y: xlabels[10],
-		    					x: ylabels[10]},
-		    					{
-			            		y: xlabels[11],
-		    					x: ylabels[11]},
-		    					{
-			            		y: xlabels[12],
-		    					x: ylabels[12]},
-		    					{
-			            		y: xlabels[13],
-		    					x: ylabels[13]},
-		    					{
-			            		y: xlabels[14],
-		    					x: ylabels[14]},
-		    					{
-			            		y: xlabels[15],
-		    					x: ylabels[15]
+    		var myChart = new Chart(ctx, {
+        	type: 'line',
+		      data: {
+		    	  labels: xlabels,
+		        datasets: 
+		       	[{
+		       		// filling data with points, Needs to be changed to a looped system
+		      		data: [{
+			      		x: xlabels[0],
+		    				y: ylabels[0]},
+		    			{
+			         	x: xlabels[1],
+		    				y: ylabels[1]},
+		    			{
+			          x: xlabels[2],
+		    				y: ylabels[2]},
+		    			{
+			          x: xlabels[3],
+		    				y: ylabels[3]},
+		    			{
+			          x: xlabels[4],
+		    				y: ylabels[4]},
+		    			{
+			          x: xlabels[5],
+		    				y: ylabels[6]},
+		    			{
+			          x: xlabels[7],
+		    				y: ylabels[7]},
+		    			{
+			          x: xlabels[8],
+		    				y: ylabels[8]},
+		    			{
+			          x: xlabels[9],
+		    				y: ylabels[9]},
+		    			{
+			          x: xlabels[10],
+		    				y: ylabels[10]},
+		    			{
+			          x: xlabels[11],
+		    				y: ylabels[11]},
+		    			{
+			          x: xlabels[12],
+		    				y: ylabels[12]},
+		    			{
+			          x: xlabels[13],
+		    				y: ylabels[13]},
+		    			{
+			          x: xlabels[14],
+		    				y: ylabels[14]},
+		    			{
+			          x: xlabels[15],
+		    				y: ylabels[15]
 		    			}],
-		                label: 'Data 1',
-		                backgroundColor: 'transparent',
-		                borderColor:'rgba(255,99,132)',
-		                borderWidth: 3
-		            }]
-		        },
-		     
-		        options: {
-		        	scales: {
-		        		xAxes: [{
-		        			type: 'time',
-		        			time: {
-		        				min: ylabels[10],
-		        				max: ylabels[0],
-		        				displayFormats: 
-		        				{
-		        					second: 'MMM D h:mm:ss a'
-		        				}
-		        			},
-		        		}]
-		        	}
+							label: 'Light Intensity',
+		          backgroundColor: 'transparent',
+		          borderColor:'rgba(255,99,132)',
+		          borderWidth: 3
+						}]
+		     	},
+		      options: {
+		      	scales: {
+		        	xAxes: [{
+		        		type: 'time',
+		        		time: {
+//									Range for data being displayed. Will be changed to 24hrs from current date
+//		        			min: xlabels[10],
+//		        			max: xlabels[0],
+		        			displayFormats: 
+		        			{
+		        				second: 'MMM D h:mm:ss a'
+		        			}
+		        		},
+		        	}]
 		        }
-
+		       }
 		    });
-
 			</script>
-	    </div>
-	    
+		</div>
 	</body>
 </html>
